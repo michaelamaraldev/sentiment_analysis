4bd92632758a5e3e740a7091c706029b021554c5
Michael Amaral <michaelzerotreze@gmail.com>
2026-05-14 20:38:00 -0300

    refactor(data): migrar pipeline de dados para o dataset de reviews do IMDb

--- data_loader.py
+++ data_loader.py
@@ -2,15 +2,17 @@ import kagglehub
 import pandas as pd
 import os
 
-print("Baixando dataset...")
-path = kagglehub.dataset_download("sahilislam007/letterbox-movie-classification-dataset")
+print("Baixando o dataset do IMDb...")
+path = kagglehub.dataset_download("lakshmi25npathi/imdb-dataset-of-50k-movie-reviews")
 
 arquivos = os.listdir(path)
-csv_nome = [f for f in arquivos if f.endswith('.csv')][0]
+csv_nome = [f for f in arquivos if f.endswith(".csv")][0]
 caminho_completo = os.path.join(path, csv_nome)
 
 df = pd.read_csv(caminho_completo)
-print("\n--- Colunas encontradas no dataset ---")
-print(df.columns.tolist())
-print("\n--- Exemplo dos dados (5 primeiras linhas) ---")
+
+print("\n--- Visualização dos Dados ---")
 print(df.head())
+
+print("\n--- Contagem de Sentimentos ---")
+print(df["sentiment"].value_counts())
